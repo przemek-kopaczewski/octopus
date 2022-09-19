d57804070f5149edc7df885712a7eb71b6f865c5
Dreagones <kopaczewski.przemek@gmail.com>
2022-09-19 13:24:46 +0200

    add persmissions to users in edit

--- app/views.py
+++ app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth import get_user_model
 from .forms import CustomUserForm
 from .models import CustomUser
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import Permission
 
 
 #main page, without login you didn't see page
@@ -14,6 +15,7 @@ def index(request):
 
 
 @login_required(login_url='login/')
+@permission_required("add_customuser")
 def create_user(request):
     form = CustomUserForm(request.POST or None)
     if form.is_valid():
@@ -30,6 +32,7 @@ def create_user(request):
     return render(request, 'register_user.html', {'form': form, 'new': True})
 
 
+@permission_required("view_customuser")
 def get_users(request):
     user = get_user_model()
     all_users = user.objects.all()
@@ -37,6 +40,7 @@ def get_users(request):
 
 
 @login_required(login_url='login/')
+@permission_required("change_customuser")
 def edit_user(request, id):
     user = get_object_or_404(CustomUser, pk=id)
     form = CustomUserForm(request.POST, instance=user)
@@ -46,13 +50,19 @@ def edit_user(request, id):
         form.last_name = form.cleaned_data['last_name']
         form.password = make_password(form.cleaned_data['password'])
         form.phone_number = form.cleaned_data['phone_number']
+        form.post_permission = request.POST.getlist('post_permission')
+        for perm in form.post_permission:
+            permission = Permission.objects.get(codename=perm)
+            user.user_permissions.add(permission)
         form.save()
         return redirect(index)
+
 
     return render(request, 'register_user.html', {'form': form, 'new': False})
 
 
 @login_required(login_url='login/')
+@permission_required("delete_customuser")
 def delete_user(request, id):
     user = get_object_or_404(CustomUser, pk=id)
 
